42d8b9b4cc1e0c3c371960a9ac7ca22d9aba36b0
martellz <286106782@qq.com>
2022-03-07 00:54:38 +0800

    add a script

---
+++ cp.sh
@@ -0,0 +1,4 @@
+cp dedicated_server_mods_setup.lua ~/dst/mods/
+cp modoverrides.lua ~/.klei/DoNotStarveTogether/MyDediServer/
+cp modoverrides.lua ~/.klei/DoNotStarveTogether/MyDediServer/Master/
+cp modoverrides.lua ~/.klei/DoNotStarveTogether/MyDediServer/Caves/
